3f5a0c2c3dbc50c83c7efceb747f8add518099f6
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-02-17 14:43:21 +0200

    Draw a triangle using vertex and fragment shaders

--- 02.hello.triangle/Main.hs
+++ 02.hello.triangle/Main.hs
@@ -1,10 +1,18 @@
 import System.Exit (exitFailure, exitSuccess)
 import Control.Monad (unless, when)
+import Foreign.Storable (sizeOf)
+import Graphics.GLUtil
+import Graphics.Rendering.OpenGL
 import Graphics.Rendering.OpenGL.Raw (glViewport, glClear, glClearColor, gl_COLOR_BUFFER_BIT)
 import qualified Graphics.UI.GLFW as GLFW
 
-mainLoop :: GLFW.Window -> IO ()
-mainLoop window = do
+vertices :: [GLfloat]
+vertices = [-0.5, -0.5, 0.0,
+             0.5, -0.5, 0.0,
+             0.0,  0.5, 0.0]
+
+mainLoop :: GLFW.Window -> ShaderProgram -> VAO -> IO ()
+mainLoop window shaderProgram vao = do
     closeFlag <- GLFW.windowShouldClose window
     unless closeFlag $ do
       GLFW.pollEvents
@@ -12,8 +20,11 @@ mainLoop window = do
       glClearColor 0.2 0.3 0.3 1.0
       glClear gl_COLOR_BUFFER_BIT
 
+      currentProgram $= Just (program shaderProgram)
+      withVAO vao $ drawArrays Triangles 0 3
+
       GLFW.swapBuffers window
-      mainLoop window
+      mainLoop window shaderProgram vao
 
 keyCallback :: GLFW.KeyCallback
 keyCallback window key _ action _ =
@@ -36,4 +47,16 @@ main = do
         GLFW.makeContextCurrent mw
         GLFW.setKeyCallback window (Just keyCallback)
         glViewport 0 0 800 600
-        mainLoop window >> GLFW.terminate >> exitSuccess
+
+        vbo <- makeBuffer ArrayBuffer vertices
+        shaderProgram <- simpleShaderProgram "02.hello.triangle/simple.vs" "02.hello.triangle/simple.fs"
+
+        vao <- makeVAO $ do
+          posn <- get (attribLocation (program shaderProgram) "position")
+          let stride = fromIntegral $ sizeOf (undefined::GLfloat) * 3
+              vad = VertexArrayDescriptor 3 Float stride offset0
+          bindBuffer ArrayBuffer $= Just vbo
+          vertexAttribPointer posn $= (ToFloat, vad)
+          vertexAttribArray posn $= Enabled
+
+        mainLoop window shaderProgram vao >> GLFW.terminate >> exitSuccess
